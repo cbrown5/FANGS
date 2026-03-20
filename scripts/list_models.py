0da9ac2c1edcfd2541896b8d3073b73ab8da7dc5
cbrown5 <c.j.brown@utas.edu.au>
2026-03-20 22:26:27 +1100

    add fangs hex

---
+++ scripts/list_models.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from google import genai
+client = genai.Client(api_key=os.environ['GEMINI_API_KEY'])
+for m in client.models.list():
+    print(m.name)
